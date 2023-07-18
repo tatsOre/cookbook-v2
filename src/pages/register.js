37a3a7835fde95424cd8ea9cdc84f8636da9d2f5
tatsOre <tatianaorezap@gmail.com>
2023-07-18 15:43:14 -0400

    Fixed missing imports error

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,5 +1,4 @@
-import Nav from '@/components/Box/Nav'
-import NavLink from '@/components/Button/NavLink'
+
 import LoginSubmission from '@/components/Login/LoginSubmission'
 import Head from 'next/head'
 import Link from 'next/link'
@@ -10,11 +9,6 @@ function Page() {
             <title>Welcome</title>
         </Head>
         <header>
-            <Nav className='nav__bar'>
-                <Link href='/' passHref legacyBehavior>
-                    <NavLink label="Cookbook" uppercase />
-                </Link>
-            </Nav>
             <main>
                 <h1 style={{ marginBlockStart: '6rem', marginBlockEnd: '0.75rem' }}>Register Page</h1>
                 <LoginSubmission />
