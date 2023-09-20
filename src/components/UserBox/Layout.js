ab3677dc73ffc30684c61fa5eeee159dd88b461c
tatsOre <tatianaorezap@gmail.com>
2023-09-19 20:38:35 -0400

    CSS styles changes

--- src/components/UserBox/Layout.js
+++ src/components/UserBox/Layout.js
@@ -14,47 +14,50 @@ function Layout({ children, user }) {
                 <NavBar className={styles['recipe__box--navigation']} fixed>
                     <MenuButton />
                     <Logo />
-                    <div className={styles.sub__menu}>
-                        {[{
-                            href: '/recipe-box',
-                            label: 'Recipes',
-                            count: user?.recipes ?? ':)',
-                            data: 'recipes',
-                            icon: <IconChefHat size={28} strokeWidth={1.35} />
-                        }, {
-                            href: '/recipe-box/favorites',
-                            label: 'Favorites',
-                            count: user?.favorites?.length ?? ':)',
-                            data: 'bookmarks',
-                            icon: <IconBookmark size={25} strokeWidth={1.75} />
-                        }, {
-                            href: '/recipe-box/shopping-lists',
-                            label: 'Shopping Lists',
-                            count: user?.shoppingLists ?? ':)',
-                            data: "shopping-lists",
-                            icon: <IconShoppingList />
-                        }].map((item, index) => {
-                            const { href, label, count, data, icon } = item
-                            return (
-                                <Link href={href} key={index}>
-                                    <div data-count={count < 100 ? count : ':)'}>
-                                        <span data-icon={data}>{icon}</span>
-                                        <span>{label}</span>
-                                    </div>
-                                </Link>
-                            )
-                        })}
-                    </div>
                 </NavBar>
             </header>
 
-            <main>
-                <div className={styles.cards__section}>
+            <div className={styles.menu__aside}>
+                {[{
+                    href: '/recipe-box',
+                    label: 'Recipes',
+                    count: user?.recipes ?? ':)',
+                    data: 'recipes',
+                    icon: <IconChefHat size={29} strokeWidth={1.35} />
+                }, {
+                    href: '/recipe-box/favorites',
+                    label: 'Favorites',
+                    count: user?.favorites?.length ?? ':)',
+                    data: 'bookmarks',
+                    icon: <IconBookmark size={26} strokeWidth={1.75} />
+                }, {
+                    href: '/recipe-box/shopping-lists',
+                    label: 'Shopping Lists',
+                    count: user?.shoppingLists ?? ':)',
+                    data: "shopping-lists",
+                    icon: <IconShoppingList />
+                }].map((item, index) => {
+                    const { href, label, count, data, icon } = item
+                    return (
+                        <Link href={href} key={index}>
+                            <div data-count={count < 100 ? count : ':)'}>
+                                <span data-icon={data}>{icon}</span>
+                            </div>
+                            {/** <span>{label}</span> */}
+                        </Link>
+                    )
+                })}
+            </div>
+            <main className={styles.cards__section}>
+                <section>
                     {children}
-                </div>
+                </section>
             </main>
+
         </React.Fragment>
     )
 }
-
+/**
+ 
+ */
 export default Layout
